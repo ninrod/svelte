6127e74373eaf77d104b1f7b471c79d81fd4d1a3
Filipe Correa Lima da Silva <filipe.silva@bcb.gov.br>
2019-06-12 18:59:09 -0300

    lesson 11: dynamic variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,25 @@
 <script>
-	let name = 'Max';
-	let age = 30;
+ let name = 'Max';
+ let age = 30;
 
-	function incrementAge() {
-		age += 1;
-	}
+ $: upper = name.toUpperCase();
+
+ function incrementAge() {
+   age += 1;
+ }
+
+ function changeName() {
+   name = 'filipe'
+ }
+
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+ h1 {
+   color: purple;
+ }
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
+<h1>Hello {upper}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change name</button>
