b0cc73e82f5978eb38d7b4b322aac92720302c7b
ninrod <filipe.silva@gmail.com>
2019-06-27 12:23:48 -0300

    17: further implement contact card

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,9 @@
 
  let name = 'Max';
  let age = 30;
+ let job = "";
+ let description = "";
+ let imgURL = "";
 
  $: upper = name.toUpperCase();
  $: console.log(name);
@@ -20,6 +23,16 @@
 
 <h1>Hello {upper}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
-<input type="text" bind:value="{name}"/>
 
-<ContactCard userName={name}/>
+<input type="text" bind:value="{name}"/>
+<input type="text" bind:value="{job}"/>
+<input type="text" bind:value="{imgURL}"/>
+<textarea type="text" bind:value="{description}"/>
+
+
+<ContactCard
+  userName={name}
+  jobTitle={job}
+  description={description}
+  imgURL={imgURL}
+/>
--- src/ContactCard.svelte
+++ src/ContactCard.svelte
@@ -1,5 +1,8 @@
 <script>
  export let userName;
+ export let jobTitle;
+ export let description;
+ export let imgURL;
 </script>
 
 <style>
@@ -58,14 +61,14 @@
 <div class="contact-card">
   <header>
     <div class="thumb">
-      <img src="" alt=""/>
+      <img src={imgURL} alt=""/>
     </div>
     <div class="user-data">
       <h1>{userName}</h1>
-      <h2>Job Title</h2>
+      <h2>{jobTitle}</h2>
     </div>
   </header>
   <div class="description">
-    <p>short description</p>
+    <p>{description}</p>
   </div>
 </div>
